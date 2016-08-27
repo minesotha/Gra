b47f019fed19e31da9aeffe249123b6f226c1dc5
minesotha <minesotha@gmail.com>
2016-08-27 17:41:34 +0200

    testowe początki

---
+++ Gra/Assets/Standard Assets/Scripts/GoNextRoom.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoNextRoom : MonoBehaviour {
+
+
+    GameObject nextRoom;
+
+    void OnCollisionEnter2D(Collision2D coli)
+    {
+        if (coli.gameObject.tag == "goRight"|| coli.gameObject.tag == "goLeft")
+        {
+            nextRoom = coli.gameObject;
+            nextRoom = nextRoom.GetComponent<goRoomGameObject>().getRoom();
+            MoveCamera();
+            MovePlayer();
+        
+        }
+    }
+
+
+    void MoveCamera()
+    {
+        Vector3 newPosition = nextRoom.transform.position;
+        Camera.main.transform.position = new Vector3(newPosition.x, newPosition.y, -15);
+    }
+
+    void MovePlayer()
+    {
+        this.transform.position = nextRoom.transform.position;
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+
+
+}
---
+++ Gra/Assets/Standard Assets/Scripts/goRoomGameObject.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class goRoomGameObject : MonoBehaviour {
+    public GameObject room;
+    
+    public GameObject getRoom()
+    {
+        return room;
+    }
+}
