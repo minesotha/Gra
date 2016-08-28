74f79b2030978e415e1f47f6ceb95f35b32cefee
minesotha <minesotha@gmail.com>
2016-08-28 16:23:43 +0200

    kursor, mechanika wskazywania obiektow, dymki

---
+++ Gra/Assets/Standard Assets/Scripts/addToEquipment.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addToEquipment : MonoBehaviour {
+
+    void OnMouseUp()
+    {
+
+        //TODO
+        this.gameObject.SetActive(false);
+    }
+
+}
---
+++ Gra/Assets/Standard Assets/Scripts/mouseOverObject.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseOverObject : MonoBehaviour {
+    SpriteRenderer sprite;
+    GameObject childText;
+
+    void Start()
+    {
+        sprite = this.GetComponent<SpriteRenderer>();
+        GetTextChild();
+    }
+
+    void OnMouseEnter()
+    {
+        sprite.color = Color.red;
+        childText.SetActive(true);
+    }
+    void OnMouseExit()
+    {
+        sprite.color = Color.white;
+        childText.SetActive(false);
+    }
+
+
+    void GetTextChild()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.tag == "textBubble")
+            {
+                childText = child.gameObject;
+            }
+        }
+    }
+
+}
